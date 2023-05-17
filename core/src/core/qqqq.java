ef1bcd79534ab22034dee3b4f033d7dea8a6df8e
jay-java <jayprajapati.tops@gmail.com>
2023-05-17 11:45:46 +0530

    calender

--- core/src/core/qqqq.java
+++ core/src/core/qqqq.java
@@ -1,5 +1,28 @@
 package core;
 
+import java.util.Scanner;
+class A extends B implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+class B{
+	
+}
+class C{
+	
+}
+
+
+
+
+
+
+
 public class qqqq {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 4; i++) {
@@ -13,6 +36,28 @@ public class qqqq {
 				System.out.println(4);
 			}
 		}
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("etner i = ");
+		int i1 = sc.nextInt();
+		switch (i1) {
+		case 1: {
+			System.out.println(1);
+			break;
+		}
+		case 2: {
+			System.out.println(2);
+			break;
+		}
+		case 3: {
+			System.out.println(3);
+			break;
+		}
+		case 4: {
+			System.out.println(4);
+			break;
+		}
+		default:
+			System.out.println("invalid");
+		}
 	}
 }
